985996e5566212b8c6c644aac5a2e58d57b3d17c
Maxwell Kendall <MaxwellKendall@users.noreply.github.com>
2019-11-08 11:35:25 -0500

    Sets up configuration for requiring CL as dependency in USAS (#1)
    * [iteration-1] plain jane storybook
    
    * [iteration1] changing location of package.json.main
    
    * [iteration-1] fixing import path
    
    * [iteration-1] parsing exports through babel
    
    * [iteration-1] changing master .css file name
    
    * updating build
    
    * [iteration-1] hiding dist folder
    
    * [iteration-1] troubleshooting prepare script
    
    * [iteration-1] using dist again
    
    * adjusting file structure
    
    * troubleshooting babel
    
    * adding dist
    
    * adding dist for sb deploy
    
    * updating deploy script
    
    * moving build to /docs

---
+++ babel.config.js
@@ -0,0 +1,29 @@
+const presets = [
+    [
+        "@babel/preset-env",
+        {
+            targets: {
+                ie: "10",
+                node: "10"
+            }
+        }
+    ],
+    "@babel/preset-react"
+];
+
+const plugins = [
+    [
+        "module-resolver",
+        {
+            root: ["src/js"]
+        }
+    ],
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-proposal-class-properties"
+];
+
+module.exports = {
+    presets,
+    plugins
+};
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'production',
+    // context: path.resolve(__dirname, "./storybook"),
+    entry: path.resolve(__dirname, 'index.js'),
+    output: {
+      path: path.resolve(__dirname, './component-lib-export'),
+      filename: 'index.js',
+      library: '',
+      libraryTarget: 'commonjs'
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+      modules: ["node_modules", path.resolve(__dirname, ".storybook/components")]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$|jsx$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/react']
+          }
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            { loader: MiniCssExtractPlugin.loader },
+            { loader: "css-loader", options: { url: false, sourceMap: true } },
+            {
+                loader: "sass-loader",
+                options: {
+                    sourceMap: true
+                }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "data-transparency-ui.css"
+      })
+    ]
+};
