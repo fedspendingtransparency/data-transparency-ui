c35ced2e8c66278029dae8ef0c8d6543e5578693
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-11-19 10:54:04 -0500

    [tooltip-polish] fixing lint errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,87 +1,87 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: 'production',
-    target: 'node',
-    entry: path.resolve(__dirname, 'index.js'),
+    mode: "production",
+    target: "node",
+    entry: path.resolve(__dirname, "index.js"),
     output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: 'index.js',
-      library: 'data-transparency-ui',
-      libraryTarget: 'commonjs',
-      publicPath: '/dist/',
-      umdNamedDefine: true
+        path: path.resolve(__dirname, "./dist"),
+        filename: "index.js",
+        library: "data-transparency-ui",
+        libraryTarget: "commonjs",
+        publicPath: "/dist/",
+        umdNamedDefine: true
     },
     resolve: {
-      extensions: [".js", ".jsx"],
-      modules: ["node_modules", path.resolve(__dirname, ".storybook/components")]
+        extensions: [".js", ".jsx"],
+        modules: ["node_modules", path.resolve(__dirname, ".storybook/components")]
     },
     module: {
-      rules: [
-        {
-          test: /\.js$|jsx$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/react']
-          }
-        },
-        {
-          test: /\.scss$/,
-          use: [
-            { loader: MiniCssExtractPlugin.loader },
-            { loader: "css-loader", options: { url: false, sourceMap: true } },
+        rules: [
             {
-              loader: "postcss-loader",
-              options: {
-                indent: 'postcss',
-                plugins: [require('autoprefixer')]
-              }
+                test: /\.js$|jsx$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env", "@babel/react"]
+                }
             },
             {
-                loader: "sass-loader"
+                test: /\.scss$/,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    { loader: "css-loader", options: { url: false, sourceMap: true } },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            indent: "postcss",
+                            plugins: [require("autoprefixer")]
+                        }
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
+                ]
             }
-          ]
-        }
-      ]
+        ]
     },
     externals: {
-      // Don't bundle react, react-dom, or lodash
-      react: {
-          commonjs: "react",
-          commonjs2: "react",
-          amd: "React",
-          root: "React"
-      },
-      "react-dom": {
-        commonjs: "react-dom",
-        commonjs2: "react-dom",
-        amd: "ReactDOM",
-        root: "ReactDOM"
-      },
-      lodash: {
-        commonjs: "lodash",
-        commonjs2: "lodash",
-        amd: "lodash",
-        root: "lodash"
-      },
-      accounting: {
-        commonjs: "accounting",
-        commonjs2: "accounting",
-        amd: "accounting",
-        root: "accounting"
-      },
-      "prop-types": {
-        commonjs: "prop-types",
-        commonjs2: "prop-types",
-        amd: "prop-types",
-        root: "prop-types"
-      }
+        // Don't bundle react, react-dom, or lodash
+        react: {
+            commonjs: "react",
+            commonjs2: "react",
+            amd: "React",
+            root: "React"
+        },
+        "react-dom": {
+            commonjs: "react-dom",
+            commonjs2: "react-dom",
+            amd: "ReactDOM",
+            root: "ReactDOM"
+        },
+        lodash: {
+            commonjs: "lodash",
+            commonjs2: "lodash",
+            amd: "lodash",
+            root: "lodash"
+        },
+        accounting: {
+            commonjs: "accounting",
+            commonjs2: "accounting",
+            amd: "accounting",
+            root: "accounting"
+        },
+        "prop-types": {
+            commonjs: "prop-types",
+            commonjs2: "prop-types",
+            amd: "prop-types",
+            root: "prop-types"
+        }
     },
     plugins: [
-      new MiniCssExtractPlugin({
-        filename: "data-transparency-ui.css"
-      })
+        new MiniCssExtractPlugin({
+            filename: "data-transparency-ui.css"
+        })
     ]
 };
