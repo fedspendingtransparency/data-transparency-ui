adf80c8af3de6eed2bad941ae977e7028c83938a
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-11-19 11:43:19 -0500

    [tooltip-polish] cleaning up styles again

--- .storybook/stories/tooltip.stories.js
+++ .storybook/stories/tooltip.stories.js
@@ -25,11 +25,18 @@ const Tooltip = () => (
       <p>Here are some random words serving the purpose of illustrating how the tooltip component works</p>
       <p>Its really pretty neat and easy to use. I think you are gonna like it a ton.</p>
       <p>You are welcome.</p>
+      <strong>Some Bold Text That is like a Header</strong>
+        <p>
+          Some text that is not apart of the header but underneath it.
+        </p>
       <ul>
         <li>Some List Item</li>
         <li>Some List Item</li>
         <li>Some List Item</li>
-        <li>Some List Item</li>
+        <li>
+          <strong>Some Bold Text That is like a Header</strong> -- 
+            Some List Item
+      </li>
       </ul>
       <p>More Paragraph</p>
     </React.Fragment>
--- components/TooltipComponent.jsx
+++ components/TooltipComponent.jsx
@@ -7,21 +7,28 @@ import "../styles/components/_tooltipComponent.scss";
 const propTypes = {
     title: PropTypes.string,
     children: PropTypes.node,
-    className: PropTypes.string
+    className: PropTypes.string,
+    textAlign: PropTypes.oneOf(['left', 'center'])
 };
 
 const defaultProps = {
     title: "Tooltip Info",
     children: <p>Here is some info.</p>,
-    className: null
+    className: null,
+    textAlign: 'left'
 };
 
 // eslint-disable-next-line import/prefer-default-export
-const TooltipComponent = (props) => (
-    <div className={cx({ [props.className]: props.className !== null })}>
-        <h1 className="tooltip__title">{props.title}</h1>
-        <div className="tooltip__text">
-            {props.children}
+const TooltipComponent = ({
+    children,
+    title,
+    className,
+    textAlign
+}) => (
+    <div className={cx({ [className]: className !== null })}>
+        <h1 className="tooltip__title">{title}</h1>
+        <div className={cx("tooltip__text", textAlign)}>
+            {children}
         </div>
     </div>
 );
