2032a0c7ed57e3e48e1a10c6793124a3b5a500d2
Maxwell Kendall <MaxwellKendall@users.noreply.github.com>
2020-01-29 11:26:34 -0500

    Migrate to MDX (#26)
    * fixing issue with the images loading in storybook
    
    * [migrate-to-mdx] using latest storybook release
    
    * [migrate-to-mdx] fix favicon
    
    * [migrate-to-mdx] migrate tooltip
    
    * [migrate-to-mdx] migrate pagination
    
    * [migrate-to-mdx] migrate quarterPicker
    
    * [migrate-to-mdx] fix styles in documentation
    
    * [migrate-to-mdx] updating pr template
    
    * [migrate-to-mdx] updating intro story
    
    * [migrate-to-mdx] update build artifacts
    
    * [migrate-to-mdx] updating pr template

---
+++ .storybook/misc.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+
+import TooltipComponent from "../components/infoTooltip/TooltipComponent";
+
+export const Tooltip = () => ( 
+    <TooltipComponent title="An Example Tooltip" textAlign={{ title: 'left', text: 'left'}}>
+      <React.Fragment>
+        <p>Here are some random words serving the purpose of illustrating how the tooltip component works</p>
+        <p>Its really pretty neat and easy to use. I think you are gonna like it a ton.</p>
+        <p>You are welcome.</p>
+        <strong>Some Bold Text That is like a Header</strong>
+          <p>
+            Some text that is not apart of the header but underneath it.
+          </p>
+        <ul>
+          <li>Some List Item</li>
+          <li>Some List Item</li>
+          <li>Some List Item</li>
+          <li>
+            <strong>Some Bold Text That is like a Header</strong> -- 
+              Some List Item
+        </li>
+        </ul>
+        <p>More Paragraph</p>
+      </React.Fragment>
+    </TooltipComponent>
+  );
+
+export const PaginationWrapper = (props) => {
+  const [page, changePage] = useState(1);
+  const [pageSize, changeLimit] = useState(10);
+
+  const handlePageChange = (pg) => {
+    console.log("changePage handler invoked w/ this parameter: ", pg);
+    changePage(pg);
+  };
+
+  const handleChangeLimit = (pageSize) => {
+    console.log("changeLimit handler invoked w/ this parameter: ", pageSize);
+    changeLimit(pageSize);
+  }
+
+  return (
+    <div className="story__container">
+      {React.cloneElement(props.children, {
+        changePage: handlePageChange,
+        currentPage: page,
+        changeLimit: handleChangeLimit,
+        pageSize,
+        ...props
+      })}
+    </div>
+  );
+};
+
+export const QuarterPickerWrapper = (props) => {
+  const [selectedQuarters, setSelectedQuarters] = useState([]); 
+
+  const handlePickQuarter = (newlySelectedQuarter) => {
+    if (selectedQuarters.includes(newlySelectedQuarter)) {
+      setSelectedQuarters(selectedQuarters.filter((quarter) => quarter !== newlySelectedQuarter))
+    }
+    else {
+      setSelectedQuarters([...new Set([...selectedQuarters, newlySelectedQuarter])])
+    }
+  }
+
+  return (
+      <div className="story__container quarter-picker-story">
+          {React.cloneElement(props.children, {
+            pickedQuarter: handlePickQuarter,
+            selectedQuarters,
+            ...props
+          })}
+      </div>
+  );
+};
--- pull_request_template.md
+++ pull_request_template.md
@@ -1,10 +1,10 @@
 Thank you for taking the time to contribute to this library.
 
 ### Check List Before Merging! (For the Author)
-- [ ] Story Book has been rebuilt (new build artifacts in `/docs`)
-- [ ] Components have been rebuilt (new build artifacts in `/dist`)
-- [ ] All new components have been exported in `/index.js`
-- [ ] Design Review Complete
-- [ ] Draft release notes created OR update release notes regarding these changes ([use this template](https://github.com/fedspendingtransparency/data-transparency-ui/blob/master/release_notes_template.md)
-- [ ] [Component Library Integration Status Report](https://github.com/fedspendingtransparency/data-act-documentation/blob/data-transparency-ui/frontend_apps/component-library-integration-status.md) updated with new component and all instances of that component in USASpending. Refer to inventory documentation [here](https://airtable.com/tblHeQOwctk5z5FOj/viw2T0ewpuDPOhHtL?blocks=show) for help with identifying instances of components. `if applicable`
+[ ] Story Book has been rebuilt (new build artifacts in `/docs`)
+[ ] Components have been rebuilt (new build artifacts in `/dist`)
+[ ] All new components have been exported in `/index.js` `(if applicable)`
+[ ] Design Review Complete
+[ ] Relevant Draft release notes have been updated describing these changes ([use this template](https://github.com/fedspendingtransparency/data-transparency-ui/blob/master/draft_release_notes.md) to document your changes) `(if applicable)`
+[ ] Integration Status Updated on relevant documentation page `(if applicable)`
 
