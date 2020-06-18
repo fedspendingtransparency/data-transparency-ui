72fcc291d0df5a0fcb4ddb7d6203ca5d1c80fd9e
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-06-18 18:32:46 -0400

    [DEV-5197] QuarterPicker Refactor:
    1. displays periods under a given quarter
    2. isActive based on cumulative selections
    3. Period and Quarter Disabled states
    
    New Props:
    
    a. periodsPerQuarter
    b. showPeriods
    c. disabledPeriods
    d. selectedPeriods

--- components/quarterPicker/QuarterButton.jsx
+++ components/quarterPicker/QuarterButton.jsx
@@ -11,65 +11,66 @@ const propTypes = {
     active: PropTypes.bool,
     quarter: PropTypes.number,
     pickedQuarter: PropTypes.func,
-    toggleTooltip: PropTypes.func
+    toggleTooltip: PropTypes.func,
+    title: PropTypes.string
 };
 
-export default class QuarterButton extends React.Component {
-    constructor(props) {
-        super(props);
+const QuarterButton = ({
+    disabled,
+    active,
+    quarter,
+    pickedQuarter,
+    toggleTooltip,
+    title = ''
+}) => {
+    const quarterTitle = title || `Q ${quarter}`;
 
-        this.onMouseLeave = this.onMouseLeave.bind(this);
-        this.onMouseEnter = this.onMouseEnter.bind(this);
-        this.clickedQuarter = this.clickedQuarter.bind(this);
-    }
-
-    onMouseEnter() {
-        if (this.props.disabled) {
-            this.props.toggleTooltip(this.props.quarter);
+    const onMouseEnter = () => {
+        if (disabled) {
+            toggleTooltip(quarter);
         }
-    }
+    };
 
-    onMouseLeave() {
-        this.props.toggleTooltip(0);
-    }
+    const onMouseLeave = () => {
+        toggleTooltip(0);
+    };
 
-    clickedQuarter(e) {
+    const clickedQuarter = (e) => {
         e.preventDefault();
-        if (!this.props.disabled) {
-            this.props.pickedQuarter(this.props.quarter);
+        if (!disabled) {
+            pickedQuarter(quarter);
         }
+    };
+
+    let additionalClasses = disabled ? 'usa-dt-quarter-picker__quarter_disabled ' : '';
+    if (quarter === 1) {
+        additionalClasses += 'usa-dt-quarter-picker__quarter_first';
+    }
+    else if (quarter === 4) {
+        additionalClasses += 'usa-dt-quarter-picker__quarter_last';
     }
 
-    render() {
-        let additionalClasses = this.props.disabled ? 'usa-dt-quarter-picker__quarter_disabled ' : '';
-        if (this.props.quarter === 1) {
-            additionalClasses += 'usa-dt-quarter-picker__quarter_first';
-        }
-        else if (this.props.quarter === 4) {
-            additionalClasses += 'usa-dt-quarter-picker__quarter_last';
-        }
-
-        if (!this.props.disabled && this.props.active) {
-            additionalClasses += ' usa-dt-quarter-picker__quarter_active';
-        }
-
-        return (
-            // Use CSS class and aria-disabled rather than disabled html property
-            // so that the disabled buttons are still focusable to display
-            // the warning tooltip
-            <button
-                className={`usa-dt-quarter-picker__quarter ${additionalClasses}`}
-                onClick={this.clickedQuarter}
-                onMouseEnter={this.onMouseEnter}
-                onFocus={this.onMouseEnter}
-                onMouseLeave={this.onMouseLeave}
-                onBlur={this.onMouseLeave}
-                aria-disabled={this.props.disabled}>
-                Q{this.props.quarter}
-            </button>
-        );
+    if (!disabled && active) {
+        additionalClasses += ' usa-dt-quarter-picker__quarter_active';
     }
-}
 
+    return (
+        // Use CSS class and aria-disabled rather than disabled html property
+        // so that the disabled buttons are still focusable to display
+        // the warning tooltip
+        <button
+            className={`usa-dt-quarter-picker__quarter ${additionalClasses}`}
+            onClick={clickedQuarter}
+            onMouseEnter={onMouseEnter}
+            onFocus={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+            onBlur={onMouseLeave}
+            aria-disabled={disabled}>
+            {quarterTitle}
+        </button>
+    );
+};
 
 QuarterButton.propTypes = propTypes;
+
+export default QuarterButton;
--- components/quarterPicker/QuarterPicker.jsx
+++ components/quarterPicker/QuarterPicker.jsx
@@ -5,46 +5,117 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import { uniqueId } from 'lodash';
+
 import QuarterButton from './QuarterButton';
 
 import '../../styles/components/quarterPicker/_quarterPicker.scss';
 
+const defaultPeriodsPerQuarter = [
+    [
+        { title: '1-2', id: '1-2' },
+        { title: '3', id: '3' }
+    ],
+    [
+        { title: '4', id: '4' },
+        { title: '5', id: '5' },
+        { title: '6', id: '6' }
+    ],
+    [
+        { title: '7', id: '7' },
+        { title: '8', id: '8' },
+        { title: '9', id: '9' }
+    ],
+    [
+        { title: '10', id: '10' },
+        { title: '11', id: '11' },
+        { title: '12', id: '12' }
+    ]
+];
+
+const isPeriodActive = (id, selectedPeriods) => selectedPeriods
+    .some((period) => parseInt(period, 10) >= parseInt(id, 10));
+
+const isPeriodDisabled = (id, disabledPeriods) => disabledPeriods
+    .some((period) => parseInt(period, 10) >= parseInt(id, 10));
+
 const propTypes = {
     selectedQuarters: PropTypes.array,
     pickedQuarter: PropTypes.func,
-    disabledQuarters: PropTypes.array
+    disabledQuarters: PropTypes.arrayOf(PropTypes.string),
+    disabledPeriods: PropTypes.arrayOf(PropTypes.string),
+    selectedPeriods: PropTypes.arrayOf(PropTypes.string),
+    showPeriods: PropTypes.bool,
+    periodsPerQuarter: PropTypes.arrayOf(
+        PropTypes.arrayOf(
+            PropTypes.shape({
+                title: PropTypes.string,
+                id: PropTypes.string
+            })
+        ))
 };
 
-
-export default class QuarterPicker extends React.Component {
-    generateQuarters() {
-        const quarters = [];
-        for (let i = 1; i <= 4; i++) {
-            quarters.push(
+const QuarterPicker = ({
+    periodsPerQuarter = defaultPeriodsPerQuarter,
+    selectedQuarters,
+    pickedQuarter,
+    disabledQuarters,
+    disabledPeriods = [],
+    showPeriods = false,
+    selectedPeriods = []
+}) => {
+    const generateButtons = () => new Array(4)
+        .fill(0)
+        .map((_, quarterIndex) => {
+            const quarterNumber = quarterIndex + 1;
+            if (showPeriods) {
+                return (
+                    <li>
+                        <p>{`Q${quarterNumber}`}</p>
+                        <ul className="usa-dt-quarter-picker__period-list">
+                            {periodsPerQuarter[quarterIndex].map((period) => (
+                                <li
+                                    className="usa-dt-quarter-picker__list-item"
+                                    key={uniqueId()}>
+                                    <QuarterButton
+                                        quarter={quarterNumber}
+                                        title={period.title}
+                                        disabled={(
+                                            isPeriodDisabled(period.id, disabledPeriods) ||
+                                            disabledQuarters.includes(quarterNumber)
+                                        )}
+                                        active={isPeriodActive(period.id, selectedPeriods)}
+                                        pickedQuarter={pickedQuarter}
+                                        toggleTooltip={() => {}} />
+                                </li>
+                            ))}
+                        </ul>
+                    </li>
+                );
+            }
+            return (
                 <li
                     className="usa-dt-quarter-picker__list-item"
-                    key={i}>
+                    key={uniqueId()}>
                     <QuarterButton
-                        quarter={i}
-                        disabled={this.props.disabledQuarters[i - 1]}
-                        active={this.props.selectedQuarters.includes(i)}
-                        pickedQuarter={this.props.pickedQuarter}
-                        toggleTooltip={this.toggleTooltip} />
+                        showPeriods={showPeriods}
+                        quarter={quarterNumber}
+                        disabled={disabledQuarters.includes(quarterNumber)}
+                        active={selectedQuarters.includes(quarterNumber)}
+                        pickedQuarter={pickedQuarter}
+                        toggleTooltip={() => {}} />
                 </li>
             );
-        }
-        return quarters;
-    }
+        });
 
-    render() {
-        return (
-            <div className="usa-dt-quarter-picker">
-                <ul className="usa-dt-quarter-picker__list">
-                    {this.generateQuarters()}
-                </ul>
-            </div>
-        );
-    }
-}
+    return (
+        <div className="usa-dt-quarter-picker">
+            <ul className="usa-dt-quarter-picker__list">
+                {generateButtons()}
+            </ul>
+        </div>
+    );
+};
 
 QuarterPicker.propTypes = propTypes;
+export default QuarterPicker;
