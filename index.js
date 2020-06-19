0fa342b2b228139c6a17bbf34075dd6d617b6afb
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-06-19 17:54:38 -0400

    [DEV-5197] exporting hook

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import Pagination from "./components/pagination/Pagination";
 import TooltipWrapper from "./components/infoTooltip/TooltipWrapper";
 import TooltipComponent from "./components/infoTooltip/TooltipComponent";
-import QuarterPicker from "./components/quarterPicker/QuarterPicker";
+import QuarterPicker, { useCumulativeQuarterPicker } from "./components/quarterPicker/QuarterPicker";
 import Picker from "./components/Picker";
 import Table from "./components/table/Table";
 
@@ -11,6 +11,7 @@ export {
     Pagination,
     TooltipWrapper,
     QuarterPicker,
+    useCumulativeQuarterPicker,
     TooltipComponent,
     Picker,
     Table
