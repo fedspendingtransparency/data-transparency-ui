6d71b8ec8c041e651daf273885cf9dc04f6080cb
Lizzie Salita <salita_elizabeth@bah.com>
2020-11-17 14:15:37 -0500

    Use message components in the table

--- components/table/Table.jsx
+++ components/table/Table.jsx
@@ -5,7 +5,12 @@
 
 import React from 'react';
 import PropTypes, { shape, oneOf, oneOfType } from 'prop-types';
-import TableContent from './TableContent';
+import { uniqueId } from 'lodash';
+import ErrorMessage from '../messages/ErrorMessage';
+import LoadingMessage from '../messages/LoadingMessage';
+import NoResultsMessage from '../messages/NoResultsMessage';
+import TableData from './TableData';
+import TableHeader from './TableHeader';
 
 require('../../styles/components/table/_table.scss');
 
@@ -26,23 +31,73 @@ const propTypes = {
 };
 
 const Table = (props) => {
+    let body;
     if (props.loading) {
-        return (
-            <div>Loading...</div>
+        body = (
+            <tr>
+                <td colSpan={props.columns.length}>
+                    <LoadingMessage />
+                </td>
+            </tr>
         );
     }
     else if (props.error) {
-        return (
-            <div>Error!</div>
+        body = (
+            <tr>
+                <td colSpan={props.columns.length}>
+                    <ErrorMessage description={props.message} />
+                </td>
+            </tr>
         );
     }
     else if (!props.rows || props.rows.length === 0) {
-        return (
-            <div>No results.</div>
+        body = (
+            <tr>
+                <td colSpan={props.columns.length}>
+                    <NoResultsMessage description={props.message} />
+                </td>
+            </tr>
         );
     }
+    else {
+        body = (<TableData {...props} />);
+    }
     return (
-        <TableContent {...props} />
+        <table className={`usda-table ${props.classNames}`}>
+            <thead className="usda-table__head">
+                <tr className="usda-table__row">
+                    {props.columns.map((col) => (
+                        <TableHeader
+                            key={uniqueId()}
+                            currentSort={props.currentSort}
+                            updateSort={props.updateSort}
+                            {...col} />
+                    ))}
+                </tr>
+                <tr className="usda-table__row">
+                    {props.columns
+                        .filter((col) => col?.subColumnNames?.length)
+                        .reduce((acc, col) => {
+                            if (col?.subColumnNames?.length) {
+                                return acc.concat(col.subColumnNames);
+                            }
+                            return acc.concat([{ ...col, displayName: '', className: 'empty-subheader' }]);
+                        }, [])
+                        .map((col) => (
+                            <TableHeader
+                                key={uniqueId()}
+                                className={col?.title ? 'nested-header' : 'empty'}
+                                currentSort={props.currentSort}
+                                updateSort={props.updateSort}
+                                {...col} />
+                        ))
+                    }
+                </tr>
+            </thead>
+            <tbody className="usda-table__body">
+                {body}
+            </tbody>
+        </table>
     );
 };
 
