acbcaa36c169c586d4c173274f533eb0836470fb
Lizzie Salita <salita_elizabeth@bah.com>
2020-11-17 14:30:59 -0500

    Added custom message stories

--- components/messages/LoadingMessage.jsx
+++ components/messages/LoadingMessage.jsx
@@ -4,19 +4,35 @@
   **/
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 require('../../styles/components/_messages.scss');
 
-const LoadingMessage = () => (
+const defaultProps = {
+    title: 'Gathering your data...',
+    description: ''
+};
+
+const propTypes = {
+    title: PropTypes.string,
+    description: PropTypes.string
+};
+
+const LoadingMessage = ({ title, description }) => (
     <div className="usda-message usda-message_loading">
         <div className="usda-message__icon">
             <FontAwesomeIcon icon="spinner" alt="loading spinner" spin />
         </div>
         <div className="usda-message__title">
-            Gathering your data...
+            {title}
+        </div>
+        <div className="usda-message__description">
+            {description}
         </div>
     </div>
 );
 
+LoadingMessage.propTypes = propTypes;
+LoadingMessage.defaultProps = defaultProps;
 export default LoadingMessage;
