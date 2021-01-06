bcd20dee851625ff7280da180768e86303703fe9
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2021-01-06 14:22:47 -0500

    [DEV-6378] Updating LoadingMessage & styles

--- components/messages/LoadingMessage.jsx
+++ components/messages/LoadingMessage.jsx
@@ -1,15 +1,67 @@
 /**
   * LoadingMessage.jsx
   * Created by Lizzie Salita 11/17/20
-  **/
+**/
 
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import GenericMessage from './GenericMessage';
+import PropTypes from 'prop-types';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
-const LoadingMessage = () => {
-    const icon = <FontAwesomeIcon icon="spinner" spin />;
-    return <GenericMessage title="Gathering your data..." icon={icon} className="loading" />;
+const LoadingMessage = ({
+    loadingText = "Gathering your data..."
+}) => (
+    <TransitionGroup>
+        <CSSTransition
+            classNames="usda-loading-animation__container"
+            timeout={{ exit: 225, enter: 195 }}
+            exit>
+            <div className="usda-loading-animation__container">
+                <div className="usda-loading-animation">
+                    <svg
+                        className="usda-loading-bars"
+                        xmlns="http://www.w3.org/2000/svg"
+                        version="1.1"
+                        width="50"
+                        height="50"
+                        style={{
+                            opacity: 0
+                        }}>
+                        <rect
+                            className="bar-one"
+                            x="0"
+                            y="0"
+                            height="50"
+                            width="10" />
+                        <rect
+                            className="bar-two"
+                            x="13"
+                            y="0"
+                            height="50"
+                            width="10" />
+                        <rect
+                            className="bar-three"
+                            x="26"
+                            y="0"
+                            height="50"
+                            width="10" />
+                        <rect
+                            className="bar-four"
+                            x="39"
+                            y="0"
+                            height="50"
+                            width="10" />
+                    </svg>
+                </div>
+                <div className="loading-message">
+                    {loadingText}
+                </div>
+            </div>
+        </CSSTransition>
+    </TransitionGroup>
+);
+
+LoadingMessage.propTypes = {
+    loadingText: PropTypes.string
 };
 
 export default LoadingMessage;
