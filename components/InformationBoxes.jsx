f1dcecaf613ef4cce3fa70f23ddc4eb5ae4e962d
Jonathan Hill <hill_jonathan@bah.com>
2021-03-30 12:07:09 -0400

    dev-6886 updates to information boxes css

--- components/InformationBoxes.jsx
+++ components/InformationBoxes.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { debounce } from 'lodash';
+import { throttle } from 'lodash';
 import { formattedValue, numberToText } from '../helpers/informationBoxesHelper';
 
 require('../styles/components/_informationBoxes.scss');
@@ -18,9 +18,10 @@ const defaultProps = {
 const InformationBoxes = ({ boxes }) => {
     const [isForTabletPortaitUp, setIsForTabletPortaitUp] = useState(window.innerWidth > 768);
 
-    const checkWidth = () => debounce(() => setIsForTabletPortaitUp(window.innerWidth > 768));
-    console.log(' Window Inner Width : ', window.innerWidth);
+    const checkWidth = throttle(() => setIsForTabletPortaitUp(window.innerWidth > 768));
+
     useEffect(() => {
+        checkWidth();
         window.addEventListener('resize', checkWidth);
         return () => window.removeEventListener('resize', checkWidth);
     }, []);
@@ -29,19 +30,20 @@ const InformationBoxes = ({ boxes }) => {
         <div className={`usa-dt-information-boxes ${numberToText[boxes.length]}-boxes`}>
             {
                 boxes.map((box) => (
-                    <div className="usa-dt-information-box">
-                        <div className="usa-dt-information-box__content">
-                            <div className="usa-dt-information-box__title">
-                                {box.title}
+                    <div key={box.title} className="usa-dt-information-box">
+                        <div className="usa-dt-information-box__divider">
+                            <div className="usa-dt-information-box__content">
+                                <div className="usa-dt-information-box__title">
+                                    {box.title}
+                                </div>
+                                <div className="usa-dt-information-box__subtitle">
+                                    {box.subtitle}
+                                </div>
+                                <div className={`usa-dt-information-box__amount${box.isLoading ? ' loading' : ''}`}>
+                                    {box.isLoading && <div className="dot-pulse" />}
+                                    {!box.isLoading && formattedValue(box.amount, box.isMonetary, isForTabletPortaitUp)}
+                                </div>
                             </div>
-                            <div className="usa-dt-information-box__subtitle">
-                                {box.subtitle}
-                            </div>
-                            <div className={`usa-dt-information-box__amount${box.isLoading ? ' loading' : ''}`}>
-                                {box.isLoading && <div className="dot-pulse" />}
-                                {!box.isLoading && formattedValue(box.amount, box.isMonetary, isForTabletPortaitUp)}
-                            </div>
-                            <div className="usa-dt-information-box__divider" />
                         </div>
                     </div>
                 ))
--- helpers/informationBoxesHelper.js
+++ helpers/informationBoxesHelper.js
@@ -1,4 +1,5 @@
-import { formatMoney, formatNumber, unitValues, unitWords, formatMoneyWithPrecision, formatNumberWithPrecision, calculateUnitForSingleValue } from './moneyFormatter';
+import { startCase } from 'lodash';
+import { formatMoney, formatNumber, unitValues, formatMoneyWithPrecision, formatNumberWithPrecision, calculateUnitForSingleValue } from './moneyFormatter';
 
 export const formattedValue = (value, isMonetary, isForTabletPortaitUp) => {
     if (value !== 0 && !value) return null;
@@ -6,7 +7,7 @@ export const formattedValue = (value, isMonetary, isForTabletPortaitUp) => {
     let formattedAmount = isMonetary ? formatMoney(value) : formatNumber(value);
     if (Math.abs(value) > unitValues.MILLION) {
         const amount = calculateUnitForSingleValue(value);
-        formattedAmount = `${isMonetary ? formatMoneyWithPrecision(value / amount.unit, 2) : formatNumberWithPrecision(value / amount.unit, 2)} ${isForTabletPortaitUp ? amount.longLabel.toUpperCase() : amount.unitLabel}`;
+        formattedAmount = `${isMonetary ? formatMoneyWithPrecision(value / amount.unit, 2) : formatNumberWithPrecision(value / amount.unit, 2)} ${isForTabletPortaitUp ? startCase(amount.longLabel) : amount.unitLabel}`;
     }
     return formattedAmount;
 };
