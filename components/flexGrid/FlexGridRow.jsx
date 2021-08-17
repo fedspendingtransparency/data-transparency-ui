ffe314460bc82628dc1dc86d52269c16dfd9549c
Stephanie Shields <shields_stephanie@bah.com>
2021-08-17 10:24:46 -0400

    Update class and prop names

--- components/flexGrid/FlexGridRow.jsx
+++ components/flexGrid/FlexGridRow.jsx
@@ -8,19 +8,19 @@ require('../../styles/components/_flexGrid.scss');
 export default function GridRow({
   children,
   className,
-  hasGaps,
-  gapSize,
+  hasGutter,
+  gutterSize,
   ...props
 }) {
-  const gapClass = hasGaps ? `grid-gap` : '';
-  const gapSizeClass = cx({
-    'grid-gap-sm': gapSize === 'sm',
-    'grid-gap-lg': gapSize === 'lg'
+  const gutterClass = hasGutter ? `usa-dt-flex-grid__gutter` : '';
+  const gutterSizeClass = cx({
+    'usa-dt-flex-grid__gutter-sm': gutterSize === 'sm',
+    'usa-dt-flex-grid__gutter-lg': gutterSize === 'lg'
   });
 
   return (
     <div
-      className={createClassString(['grid-row', gapClass, gapSizeClass, className])}
+      className={createClassString(['usa-dt-flex-grid__row', gutterClass, gutterSizeClass, className])}
       {...props}
     >
       {children}
@@ -32,15 +32,15 @@ GridRow.propTypes = {
   children: PropTypes.node.isRequired,
   /** Any additional classes to apply */
   className: PropTypes.string,
-  /** Should the row have gaps between its columns? */
-  hasGaps: PropTypes.bool,
+  /** Should the row have gutters between its columns? */
+  hasGutter: PropTypes.bool,
   /**
-   * Size of the gaps.
-   * The `hasGaps` prop must be `true` for this to have an effect.
+   * Size of the gutter.
+   * The `hasGutter` prop must be `true` for this to have an effect.
    */
-  gapSize: PropTypes.oneOf(["sm", "lg"])
+  gutterSize: PropTypes.oneOf(["sm", "lg"])
 };
 
 GridRow.defaultProps = {
-  hasGaps: false
+  hasGutter: false
 };
