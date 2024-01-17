d4c641dcbc007f1fa60bb40928dca1c9da395304
Nick Torres <nicholas.torres@kc.frb.org>
2024-01-17 15:11:26 -0700

    twitter replace

--- helpers/socialShare.jsx
+++ helpers/socialShare.jsx
@@ -3,7 +3,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faFacebookSquare,
     faLinkedin,
-    faTwitter,
     faRedditSquare
 } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope, faLink } from '@fortawesome/free-solid-svg-icons';
@@ -77,10 +76,18 @@ const GlossaryDropdownOption = ({ icon, title }) => (
     </>
 );
 
+
+const GlossaryDropdownOptionTwitter = ({ title }) => (
+    <>
+        <img src="img/social-icons/svg/x-logo.svg" alt="X (Twitter) logo" />
+        <span>{title}</span>
+    </>
+);
+
 export const socialShareOptions = [
     { component: <GlossaryDropdownOption icon={faLink} title="Copy link" />, name: `copy` },
     { component: <GlossaryDropdownOption icon={faEnvelope} title="Email" />, name: 'email' },
-    { component: <GlossaryDropdownOption icon={faTwitter} title="Twitter" />, name: 'twitter' },
+    { component: <GlossaryDropdownOptionTwitter title="X (Twitter)" />, name: 'twitter' },
     { component: <GlossaryDropdownOption icon={faFacebookSquare} title="Facebook" />, name: 'facebook' },
     { component: <GlossaryDropdownOption icon={faLinkedin} title="LinkedIn" />, name: 'linkedin' },
     { component: <GlossaryDropdownOption icon={faRedditSquare} title="Reddit" />, name: 'reddit' }
