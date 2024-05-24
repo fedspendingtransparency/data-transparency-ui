b1e5ad160e7ea2a6448d8e7d8be803c121e237b7
brianpetway <brian.petway@kc.frb.org>
2024-05-24 13:34:17 -0500

    10878 - css changes to center text in table cell, in award results table

--- components/table/TableData.jsx
+++ components/table/TableData.jsx
@@ -118,7 +118,9 @@ const TableData = ({
                                                 )}
                                             </div>
                                         )}
-                                        {data}
+                                        <div>
+                                            {data}
+                                        </div>
                                     </td>
                                 )
                         ))}
