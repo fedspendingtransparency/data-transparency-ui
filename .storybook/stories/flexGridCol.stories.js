da0cea78a4fc81ef6fc57e28336ffb8cba1f9237
Zoey Sears <zoey.sears@kc.frb.org>
2025-03-10 13:03:12 -0500

    finished added show code functionality

--- .storybook/stories/flexGridCol.stories.js
+++ .storybook/stories/flexGridCol.stories.js
@@ -7,159 +7,80 @@ export default {
   component: FlexGridCol,
 };
 
-const Template = (args) => (
-  <div className='story-flex-grid-col'>
- <FlexGridRow>
-     <FlexGridCol>
+const OneColumTemplate = (args) => (
+  <div className="story-flex-grid-col">
+  <FlexGridRow>
+      <FlexGridCol>
+        <div>column</div>
+      </FlexGridCol>
+      <FlexGridCol {...args}>
+      </FlexGridCol>
+      <FlexGridCol >
+        <div>column</div>
+      </FlexGridCol>
+    </FlexGridRow>
+  </div> 
+)
+
+const WidthTemplate = (args) => (
+  <div className="story-flex-grid-col">
+  <FlexGridRow>
+    <FlexGridCol>
       <div>column</div>
     </FlexGridCol>
-    <FlexGridCol {...args}>
+    <FlexGridCol>
+      <div>column</div>
     </FlexGridCol>
-    <FlexGridCol >
+    <FlexGridCol>
       <div>column</div>
     </FlexGridCol>
   </FlexGridRow>
-  </div>
- 
-)
-
-const EditTwoColTemplate = (value, ...args) => (
   <FlexGridRow>
-  <FlexGridCol>
-   <div>column</div>
- </FlexGridCol>
- <FlexGridCol {...args}>
- </FlexGridCol>
- <FlexGridCol {...value}>
-   <div>column</div>
- </FlexGridCol>
-</FlexGridRow>
+    <FlexGridCol {...args}/>
+    <FlexGridCol width={4}>
+      <div>column-4</div>
+    </FlexGridCol>
+  </FlexGridRow>
+  </div>
 )
 
-const RowTemplate = (args) => (
-  <div className='story-flex-grid-col'>
+const offsetTemplate = (args) => (
+  <div className="story-flex-grid-col">
+    <FlexGridRow>
+      <FlexGridCol width={{span: 8, offset: 4}}>
+        <div>colum-8 offset</div>
+      </FlexGridCol>
+    </FlexGridRow>
+    <FlexGridRow>
+      <FlexGridCol tablet={4}>
+        <div>tablet:column-4</div>
+      </FlexGridCol>
+      <FlexGridCol {...args} />
+    </FlexGridRow>
+  </div>
+)
 
+const ApplyToMultipleComponentTemplate = (args) => (
+  <div className="story-flex-grid-col">
   <FlexGridRow>
     <FlexGridCol {...args} />
+    <FlexGridCol {...args} />
+    <FlexGridCol {...args} />
   </FlexGridRow>
-  </div>
+  <FlexGridRow>
+      <FlexGridCol tablet={8}>
+        <div>tablet:column-8</div>
+      </FlexGridCol>
+      <FlexGridCol tablet={4}>
+        <div>tablet:column-4</div>
+      </FlexGridCol>
+    </FlexGridRow>
+</div>
 )
 
-export const EqualWidth = Template.bind({});
-EqualWidth.args = {
-  children: (
-    <>
-      <div>column</div>
-    </>
-  )
-}
-
-export const SettingOneColumnWidth = Template.bind({});
-SettingOneColumnWidth.args = {
-  mobile: 6,
-  children: (
-    <div>mobile:column-6</div>
-  )
-}
-
-export const VariableWidthContent = Template.bind({});
-VariableWidthContent.args = {
-  width: "auto",
-  children: (
-    <div>column-auto</div>
-  )
-}
-
-
-
-export const OffsettingColumns = () => (
-  <FlexGridRow>
-    <FlexGridCol
-      width={{
-        span: 8,
-        offset: 4,
-      }}
-    >
-      <div>column-8 offset-4</div>
-    </FlexGridCol>
-  </FlexGridRow>
-);
-
-OffsettingColumns.story = {
-  name: 'Offsetting columns',
-};
-
-export const SameAtAllBreakpoints = () => (
-  <FlexGridRow>
-    <FlexGridCol>
-      <div>column</div>
-    </FlexGridCol>
-    <FlexGridCol>
-      <div>column</div>
-    </FlexGridCol>
-    <FlexGridCol>
-      <div>column</div>
-    </FlexGridCol>
-  </FlexGridRow>
-);
-
-SameAtAllBreakpoints.story = {
-  name: 'Same at all breakpoints',
-};
-
-export const StackedColumnsAtNarrowWidths = () => (
-  <FlexGridRow>
-    <FlexGridCol tablet="fill">
-      <div>tablet:column</div>
-    </FlexGridCol>
-    <FlexGridCol tablet="fill">
-      <div>tablet:column</div>
-    </FlexGridCol>
-    <FlexGridCol tablet="fill">
-      <div>tablet:column</div>
-    </FlexGridCol>
-  </FlexGridRow>
-);
-
-StackedColumnsAtNarrowWidths.story = {
-  name: 'Stacked columns at narrow widths',
-};
-
-export const MixAndMatch = () => (
-  <FlexGridRow>
-    <FlexGridCol tablet={8}>
-      <div>tablet:column-8</div>
-    </FlexGridCol>
-    <FlexGridCol width={6} tablet={4}>
-      <div>column-6 tablet:column-4</div>
-    </FlexGridCol>
-  </FlexGridRow>
-);
-
-MixAndMatch.story = {
-  name: 'Mix and match',
-};
-
-export const ColumnWrapping = () => (
-  <FlexGridRow>
-    <FlexGridCol width={8}>
-      <div>column-8</div>
-    </FlexGridCol>
-    <FlexGridCol width={3}>
-      <div>column-3</div>
-    </FlexGridCol>
-    <FlexGridCol width={5}>
-      <div>column-5</div>
-    </FlexGridCol>
-  </FlexGridRow>
-);
-
-ColumnWrapping.story = {
-  name: 'Column wrapping',
-};
-
-export const OrderClasses = () => (
-  <FlexGridRow>
+const OrderTemplate= (args)=> (
+  <div className="story-flex-grid-col">
+      <FlexGridRow>
     <FlexGridCol
       tablet={{
         span: 4,
@@ -171,18 +92,118 @@ export const OrderClasses = () => (
     <FlexGridCol>
       <div>second, but unordered</div>
     </FlexGridCol>
-    <FlexGridCol
-      tablet={{
-        span: 4,
-        order: 'first',
-      }}
-    >
-      <div>third, but first</div>
-    </FlexGridCol>
+    <FlexGridCol {...args} />
   </FlexGridRow>
-);
+  </div>
+)
 
-OrderClasses.story = {
-  name: 'Order classes',
-};
+const StoryTemplate = (args) => (
+  <div className="story-flex-grid-col" {...args} />
+ )
 
+export const EqualWidth = OneColumTemplate.bind({});
+EqualWidth.args = {
+  children: (
+    <>
+      <div>column</div>
+    </>
+  )
+}
+
+export const SettingOneColumnWidth = OneColumTemplate.bind({});
+SettingOneColumnWidth.args = {
+  mobile: 6,
+  children: (
+    <div>mobile:column-6</div>
+  )
+}
+
+export const VariableWidthContent = OneColumTemplate.bind({});
+VariableWidthContent.args = {
+  width: "auto",
+  children: (
+    <div>column-auto</div>
+  )
+}
+
+export const SameBreakpoint = WidthTemplate.bind({});
+SameBreakpoint.args = {
+  width: 8,
+  children: (
+    <>
+      <div>column-8</div>
+    </>
+  )
+}
+
+export const OffsetColumn = offsetTemplate.bind({});
+OffsetColumn.args = {
+  tablet: {span:4, offset:4},
+  children: (
+    <div>tablet:column-10 tablet:offset-2</div>
+  )
+}
+
+export const NarrowWidth = ApplyToMultipleComponentTemplate.bind({});
+NarrowWidth.args = {
+  tablet: "fill",
+  children: (
+    <div>
+      tablet:column
+    </div>
+  )
+}
+
+export const mix = StoryTemplate.bind({});
+mix.args = {
+  children: (
+    <>
+   <FlexGridRow>
+            <FlexGridCol tablet={8}>
+                <div>tablet:column-8</div>
+            </FlexGridCol>
+            <FlexGridCol width={6} tablet={4}>
+                <div>column-6 tablet:column-4</div>
+            </FlexGridCol>
+        </FlexGridRow>
+        <FlexGridRow>
+            <FlexGridCol width={6} tablet={4}>
+                <div>column-6 tablet:column-4</div>
+            </FlexGridCol>
+            <FlexGridCol width={6} tablet={4}>
+                <div>column-6 tablet:column-4</div>
+            </FlexGridCol>
+            <FlexGridCol width={6} tablet={4}>
+                <div>column-6 tablet:column-4</div>
+            </FlexGridCol>
+        </FlexGridRow>
+        <FlexGridRow>
+            <FlexGridCol width={6}>
+                <div>column-6</div>
+            </FlexGridCol>
+            <FlexGridCol width={6}>
+                <div>column-6</div>
+            </FlexGridCol>
+        </FlexGridRow>
+    </>
+  )
+}
+
+export const ColumnWrapping = WidthTemplate.bind({});
+ColumnWrapping.args = {
+  width: 9,
+  children: (
+    <div>column-9</div>
+  )
+}
+
+export const OrderClasses = OrderTemplate.bind({});
+OrderClasses.args = {
+  tablet:{
+    span: 4,
+    order: 'first',
+  },
+  children: (
+  <div>third, but first</div>
+  )
+}
