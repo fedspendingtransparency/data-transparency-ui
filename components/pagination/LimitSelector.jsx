dd6aed40f9048f6571b5d08c9c11081eb2d4d0f5
Josue Aguilar <josue.aguilar@kc.frb.org>
2025-03-18 14:53:29 -0500

    12310 - removed default props from LimitSelector.jsx and coverted to function components

--- components/pagination/LimitSelector.jsx
+++ components/pagination/LimitSelector.jsx
@@ -13,50 +13,41 @@ const propTypes = {
     label: PropTypes.string
 };
 
-const defaultProps = {
-    pageLimit: 10,
-    limitList: [10, 25, 50, 100]
-};
-
-export default class LimitSelector extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.changeLimit = this.changeLimit.bind(this);
-    }
-
-    changeLimit(e) {
+const LimitSelector = ({
+    changeLimit: changeLimitProp,
+    pageSize = 10,
+    limitList = [10, 25, 50, 100],
+    label: labelProp
+}) => {
+    const changeLimit = (e) => {
         e.preventDefault();
         const targetId = parseInt(e.target.value, 10);
-        this.props.changeLimit(targetId);
-    }
+        changeLimitProp(targetId);
+    };
 
+    const label = labelProp || 'Rows per page: ';
+    const list = limitList.map((limit) => (
+        <option key={`limit-${limit}`} value={limit}>
+            {limit}
+        </option>
+    ));
 
-    render() {
-        const label = this.props.label ? this.props.label : 'Rows per page: ';
-        const { limitList, pageSize } = this.props;
-        const list = limitList.map((limit) => (
-            <option key={`limit-${limit}`} value={limit}>
-                {limit}
-            </option>
-        ));
-
-        return (
-            <div className="usa-dt-pagination__limit-selector__wrapper">
-                <label>
-                    {label}
-                </label>
-                <select
-                    onChange={this.changeLimit}
-                    value={pageSize}
-                    className="usa-dt-pagination__limit-selector"
-                    aria-label="limit-dropdown">
-                    {list}
-                </select>
-            </div>
-        );
-    }
-}
+    return (
+        <div className="usa-dt-pagination__limit-selector__wrapper">
+            <label>
+                {label}
+            </label>
+            <select
+                onChange={changeLimit}
+                value={pageSize}
+                className="usa-dt-pagination__limit-selector"
+                aria-label="limit-dropdown">
+                {list}
+            </select>
+        </div>
+    );
+};
 
 LimitSelector.propTypes = propTypes;
-LimitSelector.defaultProps = defaultProps;
+
+export default LimitSelector;
