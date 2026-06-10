de76ad1b88b932d93f8e1e4c66de61cb04691ac7
Nick Torres <nicholas.torres@kc.frb.org>
2026-06-10 12:59:25 -0600

    build

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import { playwright } from '@vitest/browser-playwright';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
